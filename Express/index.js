083eb0d83824a124e1d6ac3ad317c02e5cc86367
DevShain <shainaktar40@gmail.com>
2021-11-07 11:49:00 +0530

    first commit heroku

--- Express/index.js
+++ Express/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
-const bodyParser = require("body-parser");
 const port = process.env.PORT || 3001;
-const doctors = require("./routes/doctors");
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use("/doctors", doctors);
+const imp = require("./dummyDatabase");
+
+app.get("/", (req, res)=>{
+  res.send("Hello World");
+});
+
+app.get("/dummy", (req, res)=>{
+  res.send(imp);
+});
 app.listen(port, function() {
   console.log("Runnning on " + port);
 });
